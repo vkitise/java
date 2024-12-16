6f15ea6d8dad32fa00714bde22d2be7fbc9237fc
vkitise <151126627+vkitise@users.noreply.github.com>
2024-12-16 22:38:52 +0530

    Update and rename 1.java to Matrix.java

--- Matrix.java
+++ Matrix.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-class 1{
+class Matrix{
 public static void main(String[] args) {
 Scanner sc = new Scanner(System.in);
 System.out.println("Enter the size of matrices (n):");
