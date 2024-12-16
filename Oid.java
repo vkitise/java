c4ddd68b1b83d4f61f0bb5552c7f70199cfe445c
vkitise <151126627+vkitise@users.noreply.github.com>
2024-12-16 22:50:52 +0530

    Create Oid.java

---
+++ Oid.java
@@ -0,0 +1,19 @@
+
+class Outer {
+void display() {
+System.out.println("Outer class display method");
+}
+class Inner {
+void display() {
+System.out.println("Inner class display method");
+}
+}
+}
+public class Oid{
+public static void main(String[] args) {
+Outer outer = new Outer();
+outer.display();
+Outer.Inner inner = outer.new Inner();
+inner.display();
+}
+}
